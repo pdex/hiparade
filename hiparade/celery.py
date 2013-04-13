9036d739e360e361f4cf44b41b1c788e092e45d2
Chris Keating <chris.keating@skinnycorp.com>
2013-04-12 21:55:06 -0500

    celery app

---
+++ hiparade/celery.py
@@ -0,0 +1,19 @@
+from celery import Celery
+
+celery = Celery(
+    backend='mongodb://localhost/',
+    broker='amqp://hiparade:hiparade@localhost/hiparade',
+    include=[
+        'hiparade.tasks.hipchat',
+        'hiparade.tasks.partychat'
+    ],
+)
+
+celery.conf.update(
+    CELERY_ROUTES={
+        'hiparade.tasks.hipchat.received': {'queue': 'hipchat'},
+        'hiparade.tasks.hipchat.send': {'queue': 'hipchat'},
+        'hiparade.tasks.partychat.received': {'queue': 'partychat'},
+        'hiparade.tasks.partychat.send': {'queue': 'partychat'},
+    }
+)
